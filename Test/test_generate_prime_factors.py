beb77a8f9733507ba3b6bf0e8b0fb37962f6a7fa
meeshxD <lo.michelle0810@gmail.com>
2024-10-23 01:24:49 -0400

    step 1 string
    step 1: checking if string will raise Value Error

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,5 @@
-
+import pytest
+from prime import generate_prime_factors
+def test_1_string():
+    with pytest.raises(ValueError, match="Wrong data type, must be an integer"):
+        generate_prime_factors("string")
